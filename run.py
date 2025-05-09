5de3a52373b98df57fbc63c0ba4e4e5f3938ae8e
ukataria <utsavkataria05@gmail.com>
2025-05-09 14:58:28 -0400

    undo commit to run.py

--- run.py
+++ run.py
@@ -1,17 +1,16 @@
+from app import create_app
+
+app = create_app()
+
+if __name__ == "__main__":
+    app.run(debug=True)
+
 # from app import create_app
+# import os
 
 # app = create_app()
 
 # if __name__ == "__main__":
-#     app.run(debug=True)
-
-from app import create_app
-import os
-
-app = create_app()
-
-# any port from vercel will work
-if __name__ == "__main__":
-    port = int(os.environ.get("PORT", 5000))
-    app.run(host="0.0.0.0", port=port, debug=True)
+#     port = int(os.environ.get("PORT", 5000))
+#     app.run(host="0.0.0.0", port=port, debug=True)
 
